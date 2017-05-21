4053cba917f667d9cc126f7a4497179b73f2c33c
Alex GARRIDO <garridoallo@hotmail.fr>
2017-05-21 19:45:50 +0200

    Update SolveInscriptionCaptcha.py

--- Web/InscriptionCaptcha/SolveInscriptionCaptcha.py
+++ Web/InscriptionCaptcha/SolveInscriptionCaptcha.py
@@ -18,7 +18,7 @@ from PIL import Image, ImageDraw
 
 lienChall = "http://10.62.27.3/epreuve3/index.php" # Lien du formulaire de submit
 lienImg = "http://10.62.27.3/epreuve3/images/code.php" # lien de l'image , c'est ce chargement qui set le session[captcha] / le timer
-filename = r"C:\Users\Alex\Desktop\temp.png" # Place to save the captcha
+	filename = r"C:\Users\Alex\Desktop\temp.png" # Chemin de sauvegarde du captcha
 
 
 def parseImage(url):
@@ -29,13 +29,13 @@ def parseImage(url):
 	im = Image.open(url) # On ouvre l'image avec PIL
 	pixels = list(im.getdata()) # On charge les pixels (liste 1d)
 	width, height = im.size # On recup la taille de l'image
-	pixels = [pixels[i * width:(i + 1) * width] for i in range(height)] # on converti en liste 2d de pixels
+	pixels = [pixels[i * width:(i + 1) * width] for i in range(height)] # on convertit en liste 2d de pixels
 	
-	nbVerticaux = 0 # On initialise les nb de lignes
+	nbVerticaux = 0 # On initialise les nb de lignes/colonnes
 	nbHorizontaux = 0
 	
-	for elt in pixels[0]: # on parcour la premiere ligne
-		if elt == 0: # pour chaque pixel noir sur la premiere ligne, si la valeure est noire
+	for elt in pixels[0]: # On parcourt la premiere ligne
+		if elt == 0: # Pour chaques pixels noirs sur la première ligne, si la valeure est noire
 			nbVerticaux += 1 # alors on incrémente le nombre de lignes verticales
 	
 	for elt in pixels: # pour chaques lignes 
@@ -45,7 +45,7 @@ def parseImage(url):
 	return str(nbVerticaux)+str(nbHorizontaux)
 
 
-# NOM ET PRENOMS BIDON POUR L'INSCRIPTION
+# NOM ET PRENOMS POUR L'INSCRIPTION
 nomstab = ["Jean","Charles","George","Louis","Simon","Constant"]
 prenomtab = ["naej","selrahc","egroeg","siuol","nomis","tnatsnoc"]
 
@@ -62,13 +62,13 @@ for i in range(5): # Boucle de 5 comptes
 		for chunk in response.iter_content(1024):
 			f.write(chunk)
 	
-	# On set les valeurs du formulaires
+	# On set les valeurs du formulaire
 	
-	captcha = parseImage(filename) # parsing du captcha, voir parseImage()
+	captcha = parseImage(filename) # résolution du captcha, voir parseImage()
 	nom = nomstab[i]
 	prenom = prenomtab[i]
 	mail = prenom+"."+nom+"@gmail.com"
-	formulaire = "1" # parametre présent avec la val 1 sur le site
+	formulaire = "1" # param présent avec la val 1 sur le site
 
 	mondico = { # On construit le dictionnaire de param post
 	'captcha':int(captcha),
